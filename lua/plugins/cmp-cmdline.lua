d1bbbb3c9ef2981b24509973a3d3f64c1528f8d3
Alexis Flachat <alexis.flachat@gmail.com>
2026-04-25 00:10:46 +0200

    feat: Added nvim-cmp config

---
+++ lua/plugins/cmp-cmdline.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-cmdline" },
+    event = { "InsertEnter", "CmdlineEnter" },
+    opts = function(_, _)
+      local cmp = require("cmp")
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = { { name = "buffer" } },
+      })
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+        matching = { disallow_symbol_nonprefix_matching = false },
+      })
+    end,
+  },
+}
