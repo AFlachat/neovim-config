692ad68d7fcbb310eab3f28c7c8c502abca829d8
Alexis Flachat <alexis.flachat@gmail.com>
2026-04-25 04:31:33 +0200

    feat: Added keymaps to resize panes (height)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,16 +2,24 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Width-resize that also works on floating windows (e.g. Snacks explorer).
--- Overrides LazyVim's <C-Left>/<C-Right> which use `:vertical resize` (splits only).
-local STEP = 4
+-- Width/height resize that also works on floating windows (e.g. Snacks explorer).
+-- Vim's `:vertical resize` and `:resize` are splits-only.
+local STEP_W = 4
+local STEP_H = 2
 local MIN_W = 10
+local MIN_H = 3
 
 local function nudge_width(delta)
   local win = vim.api.nvim_get_current_win()
-  local cur = vim.api.nvim_win_get_width(win)
-  vim.api.nvim_win_set_width(win, math.max(MIN_W, cur + delta))
+  vim.api.nvim_win_set_width(win, math.max(MIN_W, vim.api.nvim_win_get_width(win) + delta))
 end
 
-vim.keymap.set("n", "<leader><Right>", function() nudge_width(STEP) end,  { desc = "Increase Window Width" })
-vim.keymap.set("n", "<leader><Left>",  function() nudge_width(-STEP) end, { desc = "Decrease Window Width" })
+local function nudge_height(delta)
+  local win = vim.api.nvim_get_current_win()
+  vim.api.nvim_win_set_height(win, math.max(MIN_H, vim.api.nvim_win_get_height(win) + delta))
+end
+
+vim.keymap.set("n", "<leader><Right>", function() nudge_width(STEP_W)  end, { desc = "Increase Window Width" })
+vim.keymap.set("n", "<leader><Left>",  function() nudge_width(-STEP_W) end, { desc = "Decrease Window Width" })
+vim.keymap.set("n", "<leader><Up>",    function() nudge_height(STEP_H) end, { desc = "Increase Window Height" })
+vim.keymap.set("n", "<leader><Down>",  function() nudge_height(-STEP_H) end, { desc = "Decrease Window Height" })
