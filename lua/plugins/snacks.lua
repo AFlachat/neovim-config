c7a44c03c64e3508a30d7a1c40b4b086f9ecc5d4
Alexis Flachat <alexis.flachat@gmail.com>
2026-04-25 04:09:10 +0200

    feat: Now display hidden files by default

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,6 +6,7 @@ return {
       picker = {
         sources = {
           explorer = {
+            hidden = true,
             layout = {
               preset = "sidebar",
               preview = false,
